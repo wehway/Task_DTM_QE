b9282cae2bfff059b431e0b99dde1d9cc1001415
Wahyu Purwanto <73381077+wehway@users.noreply.github.com>
2023-06-22 09:37:32 +0700

    8_introduction to Basic Programming with JAVA

---
+++ 8_Introduction to Basic Programming/problem1.java
@@ -0,0 +1,13 @@
+public class problem1 {
+    public static void main(String[] args) {
+        // input
+        float alas = 20;
+        float tinggi = 25;
+
+        // menghitung luas segitiga
+        float luas = (alas * tinggi) / 2;
+
+        // output
+        System.out.println("Luas segitiga = " + luas);
+    }
+}
